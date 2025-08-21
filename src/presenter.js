90ae827f14aef318eda2af260cd7f374a7b4faa3
Lackio-EL-14 <edsonlamas14@gmail.com>
2025-08-21 09:44:13 -0400

    feat: Ahora tambien saludamos por genero

--- src/presenter.js
+++ src/presenter.js
@@ -1,16 +1,20 @@
 import saludarSegunHora from "./saludarSegunHora";
-
+import verificarGenero from "./verificarGenero";
 
 
 const form = document.querySelector("#nombre-form");
 const input = document.querySelector("#nombre");
+const generoInput = document.querySelector("#genero");
+const generoForm = document.querySelector("#genero-form");
 const div = document.querySelector("#resultado");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstName = input.value;
+  const genero = generoInput.value;
+  const saludo = saludarSegunHora(firstName);
+  const generoMessage = verificarGenero(genero);
 
-
-  div.innerHTML = "<p>Hola " + saludarSegunHora(firstName)  + "</p>";
+  div.innerHTML = "<p>Hola " + saludo + generoMessage +  "</p>";
 });
